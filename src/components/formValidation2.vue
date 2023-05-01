430702fccaf5be479031516ec6c0090e036607f8
Yahia bassam <ybassam@2pegypt.local>
2023-05-01 15:49:46 +0300

    add | add form builder

--- src/components/formValidation2.vue
+++ src/components/formValidation2.vue
@@ -1,40 +1,49 @@
 <template>
     <b-container>
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-            <b-row v-for="question in questions" :key="question.id">
-                <div class="col-md-6">
-                    <b-form-group id="input-group-1" label="Email address:" label-for="input-1"
-                        description="We'll never share your email with anyone else.">
-                        <b-form-input id="input-1" v-model="question.form.email" type="email" placeholder="Enter email"
-                            required></b-form-input>
+        <b-form @submit.prevent="onSubmit" @reset="onReset" v-if="show">
+            <div v-for="(question, index) in questions" :key="question.id" class="mb-4 border p-5">
+                <b-row>
+                    <div class="col-md-6">
+                        <b-form-group :id="`input-group-${index}`" label="Email address:" :label-for="`input-${index}`"
+                            description="We'll never share your email with anyone else.">
+                            <b-form-input :id="`input-${index}`" v-model="question.form.email" type="email" placeholder="Enter email"
+                                required></b-form-input>
+                        </b-form-group>
+                    </div>
+                    <b-col md="6">
+    
+                        <b-form-group :id="`input-group-${index}`" label="Your Name:" :label-for="`input-${index}`">
+                            <b-form-input :id="`input-${index}`" v-model="question.form.name" placeholder="Enter name" required></b-form-input>
+                        </b-form-group>
+                    </b-col>
+    
+                    <b-form-group :id="`input-group-${index}`" label="Food:" :label-for="`input-${index}`">
+                        <b-form-select :id="`input-${index}`" v-model="question.form.food" :options="foods" required></b-form-select>
                     </b-form-group>
-                </div>
-                <b-col md="6">
-
-                    <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-                        <b-form-input id="input-2" v-model="question.form.name" placeholder="Enter name" required></b-form-input>
+    
+                    <b-form-group class="my-4" :id="`input-group-${index}`" v-slot="{ ariaDescribedby }">
+                        <b-form-checkbox-group v-model="question.form.checked" id="checkboxes-4" :aria-describedby="ariaDescribedby">
+                            <b-form-checkbox value="me">Check me out</b-form-checkbox>
+                            <b-form-checkbox value="that">Check that out</b-form-checkbox>
+                        </b-form-checkbox-group>
                     </b-form-group>
-                </b-col>
-
-                <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-                    <b-form-select id="input-3" v-model="question.form.food" :options="question.foods" required></b-form-select>
-                </b-form-group>
-
-                <b-form-group class="my-4" id="input-group-4" v-slot="{ ariaDescribedby }">
-                    <b-form-checkbox-group v-model="question.form.checked" id="checkboxes-4" :aria-describedby="ariaDescribedby">
-                        <b-form-checkbox value="me">Check me out</b-form-checkbox>
-                        <b-form-checkbox value="that">Check that out</b-form-checkbox>
-                    </b-form-checkbox-group>
-                </b-form-group>
-
-                <div class="my-3">
-                    <b-button type="submit" variant="primary" class="me-5">Submit</b-button>
-                    <b-button type="reset" variant="danger">Reset</b-button>
+    
+                    <b-card class="mt-3" header="Form Data Result">
+                        <pre class="m-0">{{ question.form }}</pre>
+                    </b-card>
+                </b-row>
+                
+                <div class="text-end">
+                    <b-button class="mt-4" variant="danger" @click="removeForm(index)">delete</b-button>
                 </div>
-                <b-card class="mt-3" header="Form Data Result">
-                    <pre class="m-0">{{ question.form }}</pre>
-                </b-card>
-            </b-row>
+            </div>
+            <div class="mb-3">
+                <b-button class="mt-4 me-5" variant="success" @click="addForm" >Add +</b-button>
+            </div>
+            <div class="mb-3">
+                <b-button type="submit" variant="primary" class="me-5">Submit</b-button>
+                <b-button type="reset" variant="danger">Reset</b-button>
+            </div>
         </b-form>
 
     </b-container>
@@ -52,15 +61,30 @@ export default {
                         food: null,
                         checked: []
                     },
-                    foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
                 },
             ],
+            foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
             show: true
         }
     },
     methods: {
-        onSubmit(event) {
-            event.preventDefault()
+        addForm(){
+            this.questions.push(
+                {
+                    form: {
+                        email: '',
+                        name: '',
+                        food: null,
+                        checked: []
+                    },
+                }
+            )
+        },
+        removeForm(index){
+            this.questions.splice(index, 1);
+        },
+        onSubmit() {
+            console.log(this.questions);
             // alert(JSON.stringify(this.form))
         },
         onReset(event) {
